e994eb3e29ef4e64b582512f63e82244440cba81
ngSgn <t105590049@ntut.org.tw>
2019-06-06 11:00:02 +0800

    Updates

---
+++ precache-manifest.b839271a4234879d8f780dd1a45ca080.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3bf76479799f62f79d849027dc1bbb",
+    "url": "/BigDataFinalFrontEnd/index.html"
+  },
+  {
+    "revision": "52f27e5822c3b11e684f",
+    "url": "/BigDataFinalFrontEnd/static/css/main.1a9148ba.chunk.css"
+  },
+  {
+    "revision": "3e068b865cddf511f80e",
+    "url": "/BigDataFinalFrontEnd/static/js/2.8bfc706b.chunk.js"
+  },
+  {
+    "revision": "52f27e5822c3b11e684f",
+    "url": "/BigDataFinalFrontEnd/static/js/main.13220a1b.chunk.js"
+  },
+  {
+    "revision": "af7a183f90f558afaf91",
+    "url": "/BigDataFinalFrontEnd/static/js/runtime~main.ccbaf553.js"
+  }
+]);
